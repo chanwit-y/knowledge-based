fe7601a6d13990d92dbaabd06f5761f54fea99cc
cy <chanwit.yimneam@hotmail.com>
2024-11-18 19:03:01 +0700

    feat: add Tailwind CSS and PostCSS configuration with custom theme enhancements

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 
 // https://vitepress.dev/reference/runtime-api#usedata
@@ -7,7 +8,7 @@ const { site, frontmatter } = useData()
 
 <template>
   <div v-if="frontmatter.home">
-    <h1>{{ site.title }}</h1>
+    <h1 class="text-red-700">{{ site.title }}</h1>
     <p>{{ site.description }}</p>
     <ul>
       <li><a href="/markdown-examples.html">Markdown Examples</a></li>
@@ -15,7 +16,21 @@ const { site, frontmatter } = useData()
     </ul>
   </div>
   <div v-else>
-    <a href="/">Home</a>
-    <Content />
+    <DefaultTheme.Layout>
+
+      <template #doc-top>
+        <!-- <div class="h-[220px] top-0 left-0 opacity-25 dark:opacity-[.55] pointer-events-none"> -->
+        <div
+          class="absolute flex flex-col z-[40] w-full !max-w-full items-center justify-center bg-transparent transition-bg overflow-hidden h-[220px] top-0 left-0 opacity-25 dark:opacity-[.55] pointer-events-none">
+          <div class="jumbo absolute opacity-60 -animated" />
+          <!-- </div> -->
+        </div>
+        <button @click="onClick">Click</button>
+        <!-- <div id="box" class="absolute top-10 left-10 w-5 h-5 bg-slate-50" /> -->
+        <!-- <Content /> -->
+      </template>
+    </DefaultTheme.Layout>
+    <!-- <a href="/">Home</a> -->
+    <!-- <Content /> -->
   </div>
 </template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,19 @@
 // https://vitepress.dev/guide/custom-theme
-import Layout from './Layout.vue'
 import type { Theme } from 'vitepress'
+import type { EnhanceAppContext } from 'vitepress'
+
+import Layout from './Layout.vue'
+import DefaultTheme from 'vitepress/theme'
+import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
+
+import '@shikijs/vitepress-twoslash/style.css'
 import './style.css'
 
 export default {
+  extends: DefaultTheme,
   Layout,
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+  enhanceApp({ app }: EnhanceAppContext) {
+    app.use(TwoslashFloatingVue)
+}
 } satisfies Theme
 
